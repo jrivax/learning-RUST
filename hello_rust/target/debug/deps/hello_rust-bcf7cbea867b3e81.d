3838e058c7b02e0b2a34f877b81a17f2429cb27a
Javier Rivas <javier.rivas@devo.com>
2022-03-13 15:08:57 +0100

    refactor: create hello world project folder and migrate code and remove unused files

---
+++ hello_rust/target/debug/deps/hello_rust-bcf7cbea867b3e81.d
@@ -0,0 +1,5 @@
+/Users/redsift/workspace/personal/learning-RUST/hello_rust/target/debug/deps/hello_rust-bcf7cbea867b3e81: src/main.rs
+
+/Users/redsift/workspace/personal/learning-RUST/hello_rust/target/debug/deps/hello_rust-bcf7cbea867b3e81.d: src/main.rs
+
+src/main.rs:
---
+++ hello_rust/target/debug/hello_rust.d
@@ -0,0 +1,1 @@
+/Users/redsift/workspace/personal/learning-RUST/hello_rust/target/debug/hello_rust: /Users/redsift/workspace/personal/learning-RUST/hello_rust/src/main.rs
