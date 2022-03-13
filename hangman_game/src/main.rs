3ca2ac1aa599614116876546c82be1d6153f31b3
Javier Rivas <javier.rivas@devo.com>
2022-03-13 17:59:26 +0100

    feat: first attemp calling function to print word

--- hangman_game/src/main.rs
+++ hangman_game/src/main.rs
@@ -4,28 +4,39 @@ fn main() {
     println!("Guess the word!");
     println!("Please input a letter to start guessing.");
     let guess_word = "hangman";
-    let mut letters_used: Vec<char> = Vec::new();
+    let mut letters_used: String = String::new();
     let guess_word_vec: Vec<char> = guess_word.chars().collect();
 
     let mut letter = String::new();
-    print_guess_word(letters_used, guess_word_vec);
+    // print_guess_word(letters_used, guess_word_vec);
     io::stdin()
       .read_line(&mut letter)
       .expect("Failed to read letter");
 
-    println!("Your guessed letter: {}", letter);
-}
-fn print_guess_word(letters_used: Vec<char>, guess_word_vec: Vec<char>) {
-  let mut print_string = String::new();
-  let mut print_string = Vec::new();
+    letters_used += &letter;
+    println!("{}", letters_used);
 
-  if(letters_used.len() == 0){
-    for letter in guess_word_vec {
-      //print_string = concat!("_ ").to_string();
-      print_string.push("_");
+    print_guess_word(letters_used, guess_word_vec);
+    // println!("Your guessed letter: {}", letter);
+}
+// FUNCTION
+fn print_guess_word(letters_used: String, guess_word_vec: Vec<char>) {
+  let mut print_string: Vec<String> = Vec::new();
+
+  if letters_used.len() == 0 {
+    for _letter in guess_word_vec {
+      print_string.push("_".to_string());
+    }
+  } else {
+    for _letter in guess_word_vec {
+      if letters_used.contains(_letter) {
+        print_string.push(_letter.to_string());
+      }else {        
+        print_string.push("_".to_string());
+      }
     }
   }
-  let s: String = print_string.connect("  ");
+  let s: String = print_string.join("  ");
 
   println!("{}", s);
 }
