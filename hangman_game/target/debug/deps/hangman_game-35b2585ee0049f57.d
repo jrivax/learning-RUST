4f55e3c63ba7d18c59021061bd9547be04edb531
Javier Rivas <javier.rivas@devo.com>
2022-03-13 15:18:24 +0100

    feat: create hangman_game project rust folder

---
+++ hangman_game/target/debug/deps/hangman_game-35b2585ee0049f57.d
@@ -0,0 +1,5 @@
+/Users/redsift/workspace/personal/learning-RUST/hangman_game/target/debug/deps/hangman_game-35b2585ee0049f57: src/main.rs
+
+/Users/redsift/workspace/personal/learning-RUST/hangman_game/target/debug/deps/hangman_game-35b2585ee0049f57.d: src/main.rs
+
+src/main.rs:
---
+++ hangman_game/target/debug/hangman_game.d
@@ -0,0 +1,1 @@
+/Users/redsift/workspace/personal/learning-RUST/hangman_game/target/debug/hangman_game: /Users/redsift/workspace/personal/learning-RUST/hangman_game/src/main.rs
