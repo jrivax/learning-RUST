f22fb7c3ffa7ae0ccc01720bdc709b5e11c3f68a
Javier Rivas <javier.rivas@devo.com>
2022-03-13 18:38:23 +0100

    feat: add loop and control if user wins !!

--- hangman_game/src/main.rs
+++ hangman_game/src/main.rs
@@ -2,26 +2,33 @@ use std::io;
 
 fn main() {
     println!("Guess the word!");
-    println!("Please input a letter to start guessing.");
+    
     let guess_word = "hangman";
-    let mut letters_used: String = String::new();
+    let letters_used: String = String::new();
     let guess_word_vec: Vec<char> = guess_word.chars().collect();
-
     let mut letter = String::new();
-    // print_guess_word(letters_used, guess_word_vec);
-    io::stdin()
-      .read_line(&mut letter)
-      .expect("Failed to read letter");
 
-    letters_used += &letter;
-    println!("{}", letters_used);
-
-    print_guess_word(letters_used, guess_word_vec);
-    // println!("Your guessed letter: {}", letter);
+    loop {
+      let mut letters_used_clone = letters_used.clone();
+      println!("Please input a letter to guess: ");
+      io::stdin()
+        .read_line(&mut letter)
+        .expect("Failed to read letter");
+  
+      letters_used_clone += &letter;
+      println!("{}", copy);
+  
+      let win = print_guess_word(copy, &guess_word_vec);
+      if win {
+        println!("You win!");
+        break;
+      }
+    }
 }
 // FUNCTION
-fn print_guess_word(letters_used: String, guess_word_vec: Vec<char>) {
+fn print_guess_word(letters_used: String, guess_word_vec: &Vec<char>) -> bool  {
   let mut print_string: Vec<String> = Vec::new();
+  let mut flag_win: bool = true;
 
   if letters_used.len() == 0 {
     for _letter in guess_word_vec {
@@ -29,14 +36,17 @@ fn print_guess_word(letters_used: String, guess_word_vec: Vec<char>) {
     }
   } else {
     for _letter in guess_word_vec {
-      if letters_used.contains(_letter) {
+      let mut _letter_copy;
+      _letter_copy = _letter.to_string();
+      if letters_used.contains(&_letter_copy) {
         print_string.push(_letter.to_string());
-      }else {        
+      }else {   
+        flag_win = false;     
         print_string.push("_".to_string());
       }
     }
   }
   let s: String = print_string.join("  ");
-
   println!("{}", s);
+  return flag_win;
 }
